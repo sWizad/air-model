b052cf207cebf00451e5cd9ea7ac532094b49a9f
sWizad <45821224+sWizad@users.noreply.github.com>
2019-01-28 17:08:12 +0700

    Add files via upload
    using slim

--- main_air2.py
+++ main_air2.py
@@ -1,15 +1,18 @@
-##
+"""
+AIR model using slim
+"""
 # Importing tensorflow
 import tensorflow as tf
 from tensorflow import keras
 import tensorflow.contrib.layers as layers
-from tensorflow.keras.datasets import mnist
+from tensorflow.keras.datasets import mnist, fashion_mnist
 # Importing some more libraries
 import numpy as np
 import matplotlib.pyplot as plt
 from transformer import transformer
-from vae import vae
-
+from vae import vae, encoder, decoder
+import tensorflow.contrib.slim as slim
+import os
 #loading the images
 #all_images = np.loadtxt('fashion-mnist_train.csv',\
 #                  delimiter=',', skiprows=1)[:,1:]
@@ -29,11 +32,23 @@ def gaussian_log_likelihood(x, mean, var, eps=1e-8):
 
 #fashion_mnist = keras.datasets.fashion_mnist
 #(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
-(train_images, train_labels), (test_images, test_labels)  = mnist.load_data()
-x_train = np.load('mnis_single.npy')
-x_train = x_train[0].astype('float32')
-train_images = train_images.astype('float32') / 255
+#(train_images, train_labels), (test_images, test_labels)  = mnist.load_data()
+#x_train = np.load('mnis_single.npy')
+#x_train = x_train[0].astype('float32')
+#x_train = np.load('fashion_sing.npy')/255
+#x_train = np.load('bmnist_sing.npy')/255
+#train_images = train_images.astype('float32') / 255
 #test_images = test_images.astype('float32') / 255
+data_type = 1
+if data_type == 1:
+    x_train = np.load('mnist_sing.npy')/255
+    y_train = np.load('nmist_label.npy')
+    (train_images, train_labels), (test_images, test_labels)  = mnist.load_data()
+elif data_type == 2:
+    x_train = np.load('fashion_sing.npy')/255
+    y_train = np.load('fashion_label.npy')
+    (train_images, train_labels), (test_images, test_labels)  = fashion_mnist.load_data()
+
 
 all_images = np.zeros((60000,50*50))
 for i in range(60000):
@@ -49,17 +64,33 @@ n_nodes_inpl = 784  #encoder
 n_nodes_hl1  = 32  #encoder
 n_nodes_hl2  = 32  #decoder
 n_nodes_outl = 784  #decoder
-rec_hidden_units = (512, 256)
-vae_generative_units=(256, 512)
+rec_hidden_units = (32, 16) #(512, 256)
+vae_generative_units= (16,32)#256, 512)
 vae_likelihood_std=0.3
 hidden_units = 64
-latent_dim = 10
+latent_dim = 5
 pic_size = 50
 win_size = 28
 
 input_layer = tf.placeholder('float32', shape=[None, pic_size*pic_size], name = "input")
+is_train = tf.placeholder(tf.bool, name='is_train')
+#rnn_out = layers.fully_connected(input_layer, pic_size*pic_size,activation_fn=tf.nn.relu,scope="hidden_rnn")
+with tf.variable_scope("hidden_rnn"):
+    x = tf.reshape(input_layer, shape=[-1, pic_size,pic_size, 1])
 
-rnn_out = layers.fully_connected(input_layer, pic_size*pic_size,activation_fn=tf.nn.relu,scope="hidden_rnn")
+    net = x
+    with slim.arg_scope(
+        [slim.conv2d, slim.fully_connected],
+        normalizer_fn=slim.batch_norm,
+        activation_fn=lambda x: tf.nn.leaky_relu(x, alpha=0.1),
+        normalizer_params={"is_training": is_train}):
+
+        for i in range(3):
+            net = slim.conv2d(net, 32, [3, 3], scope="conv%d" % (i*2+1))
+            net = slim.conv2d(net, 32, [3, 3], stride=2, scope="conv%d" % (i*2+2))
+
+        net = slim.flatten(net)
+        rnn_out = slim.fully_connected(net, 128)
 
 with tf.variable_scope("scale"):
     # sampling scale
@@ -75,7 +106,8 @@ with tf.variable_scope("scale"):
             scale_log_variance = layers.fully_connected(hidden, 1, activation_fn=None, scope=scope)
     scale_variance = tf.exp(scale_log_variance)
     scale = tf.nn.sigmoid(sample_from_mvn(scale_mean, scale_variance))
-    #scale = tf.nn.sigmoid(scale_mean)#*0 + 0.5
+    #scale = tf.nn.sigmoid(scale_mean)
+
     s = scale[:, 0]
 
 with tf.variable_scope("shift"):
@@ -85,7 +117,14 @@ with tf.variable_scope("shift"):
             hidden = layers.fully_connected(rnn_out, hidden_units*4, scope=scope)
         with tf.variable_scope("output") as scope:
             shift_mean = layers.fully_connected(hidden, 2, activation_fn=None, scope=scope)
-    shift = tf.nn.tanh(shift_mean)
+    with tf.variable_scope("log_variance"):
+        with tf.variable_scope("hidden") as scope:
+            hidden = layers.fully_connected(rnn_out, hidden_units, scope=scope)
+        with tf.variable_scope("output") as scope:
+            shift_log_variance = layers.fully_connected(hidden, 2, activation_fn=None, scope=scope)
+    shift_variance = tf.exp(shift_log_variance)
+    shift = tf.nn.tanh(sample_from_mvn(shift_mean, shift_variance))
+    #shift = tf.nn.tanh(shift_mean)
     x, y = shift[:, 0], shift[:, 1]
 
 
@@ -101,46 +140,111 @@ with tf.variable_scope("st_forward"):
         )[:, :, :, 0]
     window = tf.clip_by_value(window,0.0,1.0)
 
-with tf.variable_scope("st_bypart"):
+with tf.variable_scope("vae"):
+    window_v = tf.reshape(window,[-1,win_size,win_size,1])
+    rec_mean, rec_log_var = encoder(window_v,is_train,rec_hidden_units,latent_dim)
+
+    with tf.variable_scope("rec_sample"):
+        standard_normal_sample = tf.random_normal([tf.shape(input_layer)[0], latent_dim])
+        rec_sample = rec_mean + 1*standard_normal_sample * tf.sqrt(tf.exp(rec_log_var))
+
+    rec                   = decoder(rec_sample, is_train, [0,7,7,16],vae_generative_units, latent_dim)
+    print(rec.shape)
+    rec = tf.reshape(rec,[-1,win_size,win_size])
+    #print(window.shape)
+
+with tf.variable_scope("st_backward"):
     theta_recon = tf.stack([
         tf.concat([tf.stack([1.0 / s, tf.zeros_like(s)], axis=1), tf.expand_dims(-x / s, 1)], axis=1),
         tf.concat([tf.stack([tf.zeros_like(s), 1.0 / s], axis=1), tf.expand_dims(-y / s, 1)], axis=1),
     ], axis=1)
 
     window_recon = transformer(
+        tf.expand_dims(rec, 3),
+        theta_recon, [pic_size, pic_size]
+        )[:, :, :, 0]#*0.0
+    window_recon = tf.reshape(tf.clip_by_value(window_recon,0.0,1.0),[-1,pic_size*pic_size])
+
+with tf.variable_scope("st_bypart"):
+    theta_recon = tf.stack([
+        tf.concat([tf.stack([1.0 / s, tf.zeros_like(s)], axis=1), tf.expand_dims(-x / s, 1)], axis=1),
+        tf.concat([tf.stack([tf.zeros_like(s), 1.0 / s], axis=1), tf.expand_dims(-y / s, 1)], axis=1),
+    ], axis=1)
+
+    window_recon2 = transformer(
         tf.expand_dims(window, 3),
         theta_recon, [pic_size, pic_size]
         )[:, :, :, 0]
-    window_recon = tf.reshape(tf.clip_by_value(window_recon,0.0,1.0),[-1,pic_size*pic_size])
+    x_ttt = window_recon2
+    window_recon2 = tf.reshape(tf.clip_by_value(window_recon2,0.0,1.0),[-1,pic_size*pic_size])
 
+"""
+with tf.variable_scope("box_drawing"):
+    sx, sy = s*pic_size/2.0  , s*pic_size/2.0
+    cx, cy = (1+x)*pic_size/2.0, (1+y)*pic_size/2.0
+    lx = cx - sx
+    ly = cy - sy
+    rx = cx + sx
+    ry = cy + sy
+    lx = tf.clip_by_value((lx),0,49)
+    ly = tf.clip_by_value((ly),0,49)
+    rx = tf.clip_by_value((rx),0,49)
+    ry = tf.clip_by_value((ry),0,49)
+    x_ttt[ly,lx:rx]=0.5
+    x_ttt[ry,lx:rx]=0.5
+    x_ttt[ly:ry,lx]=0.5
+    x_ttt[ly:ry,rx]=0.5
+"""
 # output_true shall have the original image for error calculations
 output_true = tf.placeholder('float32', [None, pic_size*pic_size], name = "Truth")
+position = tf.placeholder('float32', shape=[None, 3], name = "Position")
 
-
-with tf.variable_scope("recon_loss"):
+with tf.variable_scope("recon_loss1"):
     # define our cost function
     #meansq =    tf.reduce_mean(tf.square(rec - output_true))
     meansq =    tf.reduce_mean(tf.square(window_recon - output_true))
-    meansq *= win_size*win_size
+    meansq *= win_size*win_size#*0.01
+    meansq2 =    tf.reduce_mean(tf.square(rec - window))
     binarcs = -tf.reduce_mean(
         output_true * tf.log(window_recon+ 10e-10) +
         (1.0 - output_true) * tf.log(1.0 - window_recon+ 10e-10))
-    scale_kl = 0.5 * tf.reduce_sum(tf.log(0.01)- scale_log_variance - 1.0 + scale_variance
+    binarcs *=win_size
+    scale_kl = 0.5 * tf.reduce_sum(tf.log(0.01)- scale_log_variance - 1.0 + scale_variance/0.01
                 + tf.square(scale_mean - 0.0)/0.01 , 1)
     scale_kl = tf.reduce_mean(scale_kl)
-    vae_loss = meansq-tf.reduce_mean(tf.square(window))*5+scale_kl
+    shift_kl = 0.5 * tf.reduce_sum( tf.log(2.1) - shift_log_variance - 1.0 + shift_variance/2.1 +
+        tf.square(shift_mean - 0.0)/2.1 , 1)
+    shift_kl = tf.reduce_mean(shift_kl)
+    vae_kl = 0.5 * tf.reduce_sum( tf.log(1.1) - rec_log_var - 1.0 + tf.exp(rec_log_var)/1.1 +
+        tf.square(rec_mean - 0.0)/1.1 , 1)
+    vae_kl = tf.reduce_mean(vae_kl)
+    if data_type == 1:
+        vae_loss = binarcs +scale_kl +shift_kl+vae_kl*0.01
+    elif data_type == 2:
+        vae_loss = meansq +scale_kl +shift_kl+vae_kl*0.01
 
+
+with tf.variable_scope("trick_term"):
+    # define our cost function
+    #meansq =    tf.reduce_mean(tf.square(rec - output_true))
+    trick = -tf.reduce_mean(tf.square(window))#*60*5#*.01
+    trick2 = tf.reduce_mean(tf.square(s - position[:,0]))*1
+    trick2 += 0.1*tf.reduce_mean(tf.square(x - position[:,1])+tf.square(y - position[:,2]))
+    trick_loss = meansq*0+trick2 #+scale_kl +shift_kl
+
+t_vars = tf.trainable_variables()
+vae_vars = [var for var in t_vars if 'vae' in var.name]
 # define our optimizer
-learn_rate = 0.001   # how fast the model should learn
-optimizer = tf.train.AdamOptimizer(learn_rate).minimize(vae_loss)
-#optimizer = tf.train.AdagradOptimizer(learn_rate).minimize(vae_loss)
-#optimizer2 = tf.train.AdagradOptimizer(learn_rate).minimize(vae_loss2)
-#optimizer3 = tf.train.AdagradOptimizer(learn_rate*10).minimize(vae_loss3)
+learn_rate = 0.0002   # how fast the model should learn
+
+slimopt = slim.learning.create_train_op(vae_loss, tf.train.AdamOptimizer(0.001))
+slimopt2= slim.learning.create_train_op(trick_loss, tf.train.AdamOptimizer(0.001))
 
 # initialising stuff and starting the session
 init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
+saver = tf.train.Saver()
 
 #tf.summary.FileWriter (n)
 writer = tf.summary.FileWriter("demo")
@@ -150,39 +254,26 @@ writer.add_graph(sess.graph)
 summary = tf.summary.merge([
                 tf.summary.scalar("loss", vae_loss),
                 tf.summary.scalar("mean_sq", meansq),
+                tf.summary.scalar("mean_sq2", meansq2),
                 tf.summary.scalar("binary_cross", binarcs),
                 tf.summary.scalar("scale_kl", scale_kl),
+                tf.summary.scalar("shift_kl", shift_kl),
+                tf.summary.scalar("vae_kl", vae_kl),
+                tf.summary.scalar("Tricky Term", trick2),
+                tf.summary.image("rec_window", tf.reshape(rec, [-1, win_size, win_size, 1])),
                 tf.summary.image("recon", tf.reshape(window_recon, [-1, pic_size, pic_size, 1]) ),
                 tf.summary.image("window", tf.reshape(window, [-1, win_size, win_size, 1])),
+                tf.summary.image("bypart", tf.reshape(window_recon2, [-1, pic_size, pic_size, 1]) ),
                 ])
-
-# defining batch size, number of epochs and learning rate
-batch_size = 1000  # how many images to use together for training
-hm_epochs =10  # how many times to go through the entire dataset
-tot_images = 60000 # total number of images
-# running the model for a 1000 epochs taking 100 images in batches
-# total improvement is printed out after each epoch
-kl = 0
-for epoch in range(hm_epochs):
-    epoch_loss = 0    # initializing error as 0
-    for i in range(int(tot_images/batch_size)):
-        epoch_x = all_images[ i*batch_size : (i+1)*batch_size ]
-        _,c = sess.run([optimizer,vae_loss],feed_dict={input_layer:epoch_x, output_true:epoch_x})
-        epoch_loss += c
-    epoch_x = all_images[10:300]
-    summ = sess.run(summary, feed_dict={input_layer: epoch_x, \
-       output_true: epoch_x})
-    writer.add_summary(summ,epoch)
-    if epoch%20==0:
-        print('Epoch', epoch, '/', hm_epochs, 'loss:',epoch_loss)
-
 
 
 def plot_results(model_name="vae_mnist"):
-    import os
+    #import os
     import matplotlib.pyplot as plt
+    if not os.path.exists(model_name):
+        os.makedirs(model_name)
 
-    x_true = all_images[10:30]
+    x_true = all_images[10:30].copy()
     #p = 28*2
     #x_true[:,p:p+10] = 0.8
     #x_true[14]=img.flatten()
@@ -199,19 +290,13 @@ def plot_results(model_name="vae_mnist"):
 
     for i in range(num_images):
         j = i
-
-        #if x_encoded[j].shape[0] <5:
-        #    sar = [str(int(a*10)/10) for a in x_encoded[j]]
-        #else:
-        #    sar = [str(int(a*10)/10) for a in x_encoded[j][0:4]]
-
         any_image = x_true[j]
         x_decoded,error,sh,sc = sess.run([window_recon,meansq,shift,scale],\
-                       feed_dict={input_layer:[any_image], output_true:[any_image]})
+                       feed_dict={input_layer:[any_image], output_true:[any_image], is_train: False})
         x_dec = x_decoded[0].reshape(pic_size, pic_size)
 
         #print(x_decoded)
-        print(sc[0],sh[0])
+        #print(sc[0],sh[0])
         sc[0] = sc[0]
         sh[0] = sh[0]
         sx, sy = sc[0,0]*pic_size/2.0  , sc[0,0]*pic_size/2.0
@@ -248,6 +333,40 @@ def plot_results(model_name="vae_mnist"):
         ax.get_yaxis().set_visible(False)
 
     plt.savefig(filename)
-    plt.show()
+    plt.close('all')
+    #plt.show()
+
+# defining batch size, number of epochs and learning rate
+batch_size = 256   # how many images to use together for training
+hm_epochs = 71     # how many times to go through the entire dataset
+tot_images = 60000 # total number of images
+
+kl = 0
+for epoch in range(hm_epochs):
+    epoch_loss = 0    # initializing error as 0
+    for i in range(int(tot_images/batch_size)):
+        epoch_x = all_images[ i*batch_size : (i+1)*batch_size ]
+        posi    = y_train[ i*batch_size : (i+1)*batch_size ]
+        #_,c = sess.run([optimizer2,vae_loss],feed_dict={input_layer:epoch_x, output_true:epoch_x})
+        if (epoch<1 or (epoch%5<=21 and epoch<31) or (epoch%5==0 and epoch<61)):
+            _,c = sess.run([slimopt2,vae_loss],feed_dict={input_layer:epoch_x,
+                  output_true:epoch_x , is_train:True, position:posi})
+        else:
+            _,c = sess.run([slimopt,vae_loss],feed_dict={input_layer:epoch_x, output_true:epoch_x, is_train:True})
+
+        epoch_loss += c
+    epoch_x = all_images[10:300]
+    posi    = y_train[10:300]
+    summ = sess.run(summary, feed_dict={input_layer: epoch_x, \
+       output_true: epoch_x, is_train: False, position:posi})
+    writer.add_summary(summ,epoch)
+    if epoch%2==0:
+        print('Epoch', epoch, '/', hm_epochs, 'loss:',epoch_loss)
+        #plot_results(model_name=("vae_test/"+str(epoch)))
+    if epoch%100 == 0:
+        if not os.path.exists('./model'):
+            os.makedirs('./model')
+        saver.save(sess, './model/' + str(i))
 
 plot_results(model_name="vae_test")
+#plt.show()
